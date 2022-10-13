56b72ce00295b202153d321b46188e143eda367b
Kazi Md Minhajul Haider <kaziminhajulhaider@gmail.com>
2022-10-13 23:23:49 +0600

    mobx-counter-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { observer } from "mobx-react-lite"
 
-function App() {
+const App = observer(({counter}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={()=> counter.increaseCount()}>+</button>
+      <p>{counter.count}</p>
+      <button onClick={() => counter.decreaseCount()}>-</button>
     </div>
   );
-}
+});
 
 export default App;
